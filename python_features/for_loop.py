d59b05cefba491a4d13c154a2f4d6d442597e865
Luiso <lecc628@gmail.com>
2022-06-17 07:26:45 +0200

    Create for loop that not execute else clause

--- python_features/for_loop.py
+++ python_features/for_loop.py
@@ -13,5 +13,21 @@ def for_loop_executing_else_clause() -> None:
     else:  # The 'else' clause applies to 'while' loop, too.
         print("Enters 'else' statement of 'for' loop.")
 
+# Output:
+# a
+# b
+
+def for_loop_not_executing_else_clause() -> None:
+    letters_list = ['a', 'b', 'c']
+    letter_stop = 'c'
+    
+    for letter in letters_list:
+        if letter == letter_stop:
+            break
+        print(letter)
+    else:  # The 'else' clause applies to 'while' loop, too.
+        print("Enters 'else' statement of 'for' loop.")
 
 for_loop_executing_else_clause()
+print()
+for_loop_not_executing_else_clause()
