5ebb781577e6bec7642fade54afe0960c5913e94
Luiso <lecc628@gmail.com>
2022-06-25 06:49:19 +0200

    Compare f-string and str.format approaches

---
+++ python_features/compare_f_string_and_str_format.py
@@ -0,0 +1,16 @@
+
+def compare_f_string_and_str_format(num = 123.456, width = 6, decimals = 1, /):
+    '''Compares f-string and str.format approaches.'''
+    str_1 = f'{num = :6.1f}'
+    print(str_1)
+    str_2 = f'{num = :{width}.{decimals}f}'
+    print(str_2)
+
+    str_3 = 'num = {0:6.1f}'.format(num)
+    print(str_3)
+    str_4 = 'num = {0:{1}.{2}f}'.format(num, width, decimals)
+    print(str_4)
+
+compare_f_string_and_str_format()
+print()
+compare_f_string_and_str_format(1.6789, 3, 2)
