76affaa0e31b11ceec1dee5dbededcab1f3f5b9f
Luiso <lecc628@gmail.com>
2022-07-23 07:09:31 +0200

    Use try-except-else-finally statement

---
+++ python_features/try_except_else_finally.py
@@ -0,0 +1,25 @@
+
+def divide(x: float, y: float) -> float:
+    try:
+        result = x / y
+    except ZeroDivisionError:  #except ZeroDivisionError as err:
+        print("Division by zero!")  #print(f"There was an error: {err}")
+    else:
+        print("Result is", result)
+    finally:
+        print("Executing finally clause.")
+
+divide(2, 1)
+# Result is 2.0
+# Executing finally clause.
+
+divide(2, 0)
+# Division by zero!
+# Executing finally clause.
+
+divide("2", "1")
+# Executing finally clause.
+# Traceback (most recent call last):
+#   File "<stdin>", line 2, in <module>
+#   File "<stdin>", line 4, in divide
+# TypeError: unsupported operand type(s) for /: 'str' and 'str'
