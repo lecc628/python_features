de77693c179ebf5eea9fc2611bf6aca1e27271b2
Luiso <lecc628@gmail.com>
2022-07-30 07:00:07 +0200

    Use iterable packing and unpacking
    It shows examples for iterable packing and unpacking using
    the regular and extended forms.

---
+++ python_features/iterable_packing_unpacking.py
@@ -0,0 +1,29 @@
+# In function parameters:
+# When you use *args and **kwargs, they have the tuple and dict types respectively.
+
+# In iterable unpacking:
+# When you use *values, it has the list type.
+
+from typing import Any, List
+
+px: float = 1.0
+py: float = 2.0
+pz: float = 3.0
+
+name: str = "Luis Enrique"
+phone: str = "+34 123 45 67 89"
+rest: List[Any] = ["email@gmail.com", "Address 1", "Address 2"]  # It can be any iterable form (tuple, list, dict, etc.).
+
+# Iterable packing (regular or extended forms):
+pos = px, py, pz  # regular form
+info = name, phone, *rest  # extended form
+
+print(pos)
+print(info)
+print()
+
+# Iterable unpacking (regular or extended forms):
+var_px, var_py, var_pz = pos  # regular form
+var_name, var_phone, *var_rest = info  # extended form (the type of var_rest is list).
+
+print(var_px, var_py, var_pz, var_name, var_phone, var_rest, sep = "\n")
