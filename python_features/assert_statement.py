36185b12dec4f5cd1f62e7704069044e04115c31
Luiso <lecc628@gmail.com>
2022-08-13 07:15:23 +0200

    Improve assert statement use

--- python_features/assert_statement.py
+++ python_features/assert_statement.py
@@ -1,6 +1,17 @@
 
-a = 5
+# - The assertions are evaluated if the __debug__ constant value is True. By default its value is True.
+# - If you execute the interpreter with the option -O (optimization), then the __debug__ constant value
+# is False and the assertions are ignored.
 
-assert a > 5, f'The value of "a" must be greater than {a}.'
+MIN_VALUE: int = 8
 
+a: int = 5
 print(a)
+
+assert a > MIN_VALUE, f'The value of "a" must be greater than {MIN_VALUE}.'
+
+# The above assertion is semantically equivalent to the following code:
+
+if __debug__:
+    if not a > MIN_VALUE:
+        raise AssertionError(f'The value of "a" must be greater than {MIN_VALUE}.')
